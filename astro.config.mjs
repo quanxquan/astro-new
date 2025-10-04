8612517cf72c9e933c3927f10a0cdc10a44f884a
Quan <haojs.uk@outlook.com>
2025-10-04 20:33:48 +0800

    完善功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+    site: 'https://glowing-seahorse-6283ed.netlify.app'
+});
